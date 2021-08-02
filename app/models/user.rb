08d7baa83aed8dd606f48d560d2e9db08de41c26
omokun <omokun1215@gmail.com>
2021-08-02 11:24:55 +0900

    レビュー訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true,
-               format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
-                         message: 'Please use double-byte characters (kanji, hiragana, and katakana)' } do
-    validates :last_name
-    validates :first_name
-  end
-
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: 'Please use double-byte characters (katakana)' } do
-    validates :last_name_kana
-    validates :first_name_kana
-  end
-
   with_options presence: true do
     validates :nickname
     validates :birthday
-  end
 
-  validates :password, presence: true,
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'Please include both single-byte alphanumeric characters.' }
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                           message: 'Please use double-byte characters (kanji, hiragana, and katakana)' } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶ]+\z/, message: 'Please use double-byte characters (katakana)' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+
+    with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                           message: 'Please include both single-byte alphanumeric characters.' } do
+      validates :password
+    end
+  end
 
   # has_many :items, dependent: :destroy
   # has_many :purchase_records, dependent: :destroy
