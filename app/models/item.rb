88457ab8342dd55876defbcbad2b3393c8fb0de2
omokun <omokun1215@gmail.com>
2021-08-03 21:45:28 +0900

    itemモデル単体テスト

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,7 @@ class Item < ApplicationRecord
     validates :item_description
 
     with_options format: { with: /\A[0-9]+\z/, message: 'Only single-byte numbers can be used.' } do
-      validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 10000000}
+      validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000}
     end
   
     with_options numericality: { other_than: 1 , message: "can't be blank"} do
@@ -20,11 +20,11 @@ class Item < ApplicationRecord
 end
 
   belongs_to :user
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_cost
-  belongs_to :area
-  belongs_to :shipping_time
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :area
+  belongs_to_active_hash :shipping_time
   has_one_attached :image
   # has_one :purchase_record
 end
