0bc2559f24c308d57e19c5eb2f2518151da24649
omokun <omokun1215@gmail.com>
2021-08-03 17:22:44 +0900

    商品出品機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!, except: [:show, :index]
+  before_action :authenticate_user!, except: [:show, :index]
 
   def index
   end
@@ -9,6 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :item_description, :category_id, :condition_id, :shipping_cost_id, :area_id, :shipping_time_id, :price).merge(user_id: current_user.id)
+  end
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,31 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :item_description
+
+    with_options format: { with: /\A[0-9]+\z/, message: 'Only single-byte numbers can be used.' } do
+      validates :price, numericality: { only_integer: true, greater_than: 300, less_than: 10000000}
+    end
   
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_cost_id
+      validates :area_id
+      validates :shipping_time_id
+    end
+end
+
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :area
+  belongs_to :shipping_time
+  has_one_attached :image
   # has_one :purchase_record
 end
+
