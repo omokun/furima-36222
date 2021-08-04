1ba255e7a545bce822b75002ce80819b341f99ab
omokun <omokun1215@gmail.com>
2021-08-04 11:28:11 +0900

    不要な部分をコメントアウト

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,9 @@ class Item < ApplicationRecord
     validates :name
     validates :item_description
 
-    with_options format: { with: /\A[0-9]+\z/, message: 'Only single-byte numbers can be used.' } do
+    # with_options format: { with: /\A[0-9]+\z/, message: 'Only single-byte numbers can be used.' } do
       validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
-    end
+    # end
 
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
