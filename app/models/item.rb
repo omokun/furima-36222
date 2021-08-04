afafa7d0d995d474ab778dcd185e2754028dad55
omokun <omokun1215@gmail.com>
2021-08-04 11:50:37 +0900

    コメントアウト削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,10 +5,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :item_description
-
-    # with_options format: { with: /\A[0-9]+\z/, message: 'Only single-byte numbers can be used.' } do
-      validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
-    # end
+    validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10_000_000 }
 
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -97,16 +97,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than 10000000')
       end
-      # it 'priceは半角英字のみでは登録できない' do
-        # @item.price = 'abc'
-        # @item.valid?
-        # expect(@item.errors.full_messages).to include('Price is not a number')
-      # end
-      # it 'price半角英数混合では登録できない' do
-        # @item.price = 'abc321'
-        # @item.valid?
-        # expect(@item.errors.full_messages).to include('Price is not a number')
-      # end
       it 'userが空では登録できない' do
         @item.user = nil
         @item.valid?
