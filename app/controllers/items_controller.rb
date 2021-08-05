d103df060070165586180abc5a3855a51a8971b0
omokun <omokun1215@gmail.com>
2021-08-05 12:06:38 +0900

    レビュー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :redirect_to_index , only: [:edit, :update]
 
   def index
     @item = Item.all.order(created_at: :desc)
@@ -19,16 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless current_user == @item.user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -37,6 +35,14 @@ class ItemsController < ApplicationController
   end
 
   private
+
+  def redirect_to_index
+    redirect_to action: :index unless current_user == @item.user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :name, :item_description, :category_id, :condition_id, :shipping_cost_id, :area_id,
